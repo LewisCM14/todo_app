95a681df4c66555afc51fa34927ba7b3c4f343d2
LewisCM14 <Lewiscmartin@live.co.uk>
2022-11-29 22:09:06 +0000

    create POST and GET functions for todo model

--- todo/main.py
+++ todo/main.py
@@ -1,28 +1,34 @@
-from typing import Optional
-
-from fastapi import FastAPI
-from config import settings
-
-from todo.models.models import Todo, Todo_Pydantic, ToDoIn_Pydantic
+from typing import Optional, List
+ 
+from fastapi import FastAPI, HTTPException
+from todo.models.models import Todo, Todo_Pydantic, TodoIn_Pydantic
 from pydantic import BaseModel
+
 from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 
-
+from config import settings
 app = FastAPI()
+
+class Status(BaseModel):
+    message: str
 
 @app.get('/')
 async def read_root():
     return {"Hello": "World"}
 
-class Status(BaseModel):
-    message: str
+"""
+using the todo model return a list of all items asynchronously
+"""
+@app.get('/todos', response_model=List[Todo_Pydantic])
+async def get_todos():
+    return await Todo_Pydantic.from_queryset(Todo.all())
 
 """
 set the url and model for post request
 async create the todo object and await from orm
 """
-@app.post('/todos', response_model=Todo_Pydantic)
-async def create_todo(todo: ToDoIn_Pydantic):
+@app.post("/todos", response_model=Todo_Pydantic)
+async def create_todo(todo: TodoIn_Pydantic):
     todo_obj = await Todo.create(**todo.dict(exclude_unset=True))
     return await Todo_Pydantic.from_tortoise_orm(todo_obj)
 
@@ -30,7 +36,7 @@ async def create_todo(todo: ToDoIn_Pydantic):
 register_tortoise(
     app,
     db_url=settings.DATABASE_URL,
-    modules={'models': ["todo.models.models"]},
+    modules={"models": ["todo.models.models"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
--- todo/todo/models/models.py
+++ todo/todo/models/models.py
@@ -1,13 +1,15 @@
+
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 class Todo(models.Model):
     """
     The model to hold the todo items
+    Defining `id` field is optional, it will be defined automatically
     """
     id = fields.IntField(pk=True)
     title = fields.CharField(max_length=255)
     completed = fields.BooleanField()
-
+    
 Todo_Pydantic = pydantic_model_creator(Todo, name="Todo")
-ToDoIn_Pydantic = pydantic_model_creator(Todo, name="TodoIn", exclude_readonly=True)
+TodoIn_Pydantic = pydantic_model_creator(Todo, name="TodoIn", exclude_readonly=True)
